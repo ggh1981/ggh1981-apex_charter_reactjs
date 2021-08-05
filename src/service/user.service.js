1fa8ab50a4731b845d8d544f36efb619c96f175d
Guillermo Gomez <ggh1981@hotmail.com>
2021-08-05 00:32:50 -0500

    Address Modified

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://127.0.0.1:8080/api/';
+const API_URL = 'http://167.114.114.176:8080/api/';
 
 class UserService {
   getPublicContent() {
