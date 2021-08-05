4951be6dc6ec5f804623c6cae69c5b57ce923624
Guillermo Gomez <ggh1981@hotmail.com>
2021-08-05 00:44:46 -0500

    Initial Commit

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = 'http://167.114.114.176:8080/api/auth/';
 
 class AuthService {
   login(username, password) {
